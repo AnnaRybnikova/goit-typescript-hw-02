81098e2a061824d45d9b8895645f289382675923
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2024-11-23 03:58:07 +0100

    message error style done

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import s from './ErrorMessage.module.css'
 
 const ErrorMessage = ({ children }) => {
   return (
-      <div>
-          <h2>{children}</h2>
+      <div className={s.error_msg_container}>
+          <h2 className={s.error_header}>{children}</h2>
       </div>
   )
 }
